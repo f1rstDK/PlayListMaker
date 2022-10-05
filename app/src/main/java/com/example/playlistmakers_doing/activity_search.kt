daaf6b9f005ebefca17d54ac39610b99438ae0c6
DenisKotik <stores.steamcommunity@mail.ru>
2022-10-05 09:28:38 +0300

    This is my all homework

---
+++ app/src/main/java/com/example/playlistmakers_doing/activity_search.kt
@@ -0,0 +1,12 @@
+package com.example.playlistmakers_doing
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_search : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
