9953dbb463cd8b4629ead7d9ae28ca09f16ac3d7
DenisKotik <stores.steamcommunity@mail.ru>
2022-10-20 07:40:24 +0300

    Homework #1. Sprint #8

--- app/src/main/java/com/example/playlistmakers_doing/ActivitySearch.kt
+++ app/src/main/java/com/example/playlistmakers_doing/ActivitySearch.kt
@@ -4,7 +4,7 @@ package com.example.playlistmakers_doing
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class activity_search : AppCompatActivity() {
+class ActivitySearch : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
--- app/src/main/java/com/example/playlistmakers_doing/MainActivity.kt
+++ app/src/main/java/com/example/playlistmakers_doing/MainActivity.kt
@@ -3,9 +3,7 @@ package com.example.playlistmakers_doing
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         val activity_search = findViewById<Button>(R.id.search)
 
         activity_search.setOnClickListener {
-            val search_intent = Intent(this, com.example.playlistmakers_doing.activity_search::class.java)
+            val search_intent = Intent(this, com.example.playlistmakers_doing.ActivitySearch::class.java)
             startActivity(search_intent)
         }
 
